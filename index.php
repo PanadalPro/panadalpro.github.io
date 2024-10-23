1a3b97130d4d46030a04fc40784838aade62816c
PanadalPro <nico@paupalacios.com>
2024-10-23 20:41:05 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>TecnoThau</title>
+	</head>
+	<body>
+		<h1>TecnoThau</h1>
+	</body>
+</html>
